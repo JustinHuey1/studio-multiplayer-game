fb86639553ece90a345ce57c5e1444203bfd7956
Student <student@Students-MacBook-Air.local>
2020-04-22 18:48:45 -0700

    Added Pictionary component

--- src/gameData.js
+++ src/gameData.js
@@ -13,6 +13,7 @@ import TwentyOneQuestions from './games/twentyOneQuestions/TwentyOneQuestions.js
 import burst_Forth from "./games/Burst_Forth/burstForth.js";
 import fight from "./games/fight/fight.js";
 import uno from './games/uno/uno.js';
+import Pictionary from './games/pictionary/Pictionary.js';
 
 const gameData = {
 
@@ -23,6 +24,15 @@ const gameData = {
     minUsers: 1,
     maxUsers: 10,
     component: ChatRoom,
+  },
+
+  tictactoe: {
+    title: "Pictionary",
+    authors: "Justin Huey",
+    description: "Draw and guess",
+    minUsers: 2,
+    maxUsers: 5,
+    component: Pictionary,
   },
 
   tictactoe: {
