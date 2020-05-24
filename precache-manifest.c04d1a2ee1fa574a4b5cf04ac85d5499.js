0db5118219aed1426b4f36cc0d0ee7a03319bbb1
Student <student@Students-MacBook-Air.local>
2020-05-24 14:14:44 -0700

    Updates

---
+++ precache-manifest.c04d1a2ee1fa574a4b5cf04ac85d5499.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deee212f0ce30ca9cd5e805e3aab2d82",
+    "url": "/studio-multiplayer-game/index.html"
+  },
+  {
+    "revision": "cd25e861fde3b48b3944",
+    "url": "/studio-multiplayer-game/static/css/main.328a37ae.chunk.css"
+  },
+  {
+    "revision": "c1f793331bcfe87d9921",
+    "url": "/studio-multiplayer-game/static/js/2.6617d526.chunk.js"
+  },
+  {
+    "revision": "66d00bf2297b91423804447ee8ad9791",
+    "url": "/studio-multiplayer-game/static/js/2.6617d526.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd25e861fde3b48b3944",
+    "url": "/studio-multiplayer-game/static/js/main.124534f1.chunk.js"
+  },
+  {
+    "revision": "fb109612cc225cf025c1",
+    "url": "/studio-multiplayer-game/static/js/runtime-main.558293e5.js"
+  },
+  {
+    "revision": "4454a02b6285aa53d97ba3f3c9641c1c",
+    "url": "/studio-multiplayer-game/static/media/Logo.4454a02b.png"
+  },
+  {
+    "revision": "1687bbe57a7131a4f6cfeac45f289475",
+    "url": "/studio-multiplayer-game/static/media/Whisper icon.1687bbe5.png"
+  }
+]);
